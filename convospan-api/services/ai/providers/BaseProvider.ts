2fe2b58659ac6193a63e9d516ca0391d335d048b
Z-SIS <sid@stepupdigiworld.com>
2026-03-16 19:01:20 +0530

    Remove unused convospan-api services

--- convospan-api/services/ai/providers/BaseProvider.ts
+++
@@ -1,7 +0,0 @@
-
-import { ModelRequest, ModelResponse, LLMProvider } from "../types.js";
-
-export abstract class BaseProvider {
-    abstract getProviderName(): LLMProvider;
-    abstract generate(request: ModelRequest): Promise<ModelResponse>;
-}
--- convospan-api/services/ai/providers/index.ts
+++
@@ -1,7 +0,0 @@
-
-export * from "./BaseProvider.js";
-export * from "./OpenAIProvider.js";
-export * from "./GeminiProvider.js";
-export * from "./GroqProvider.js";
-export * from "./AnthropicProvider.js";
-export * from "./OllamaProvider.js";
